b23919aee38a5d172a965058ba89e9db26a93b39
Rodrigo Galvan <rodrigogalvann1@gmail.com>
2024-12-10 21:03:54 -0300

    Fixing some bugs

---
+++ pages/InventoryPage.ts
@@ -0,0 +1,16 @@
+import { Locator, Page } from "@playwright/test"
+
+export default class InventoryPage {
+
+
+    constructor(private page: Page){}
+
+    async addCartRandomItem(){
+        const allItems = await this.page.locator('.inventory_item').all();
+        const anItem = allItems[Math.floor(Math.random() * allItems.length)];
+        await anItem.locator('.btn_primary').click()
+        return anItem
+    }
+
+
+}
